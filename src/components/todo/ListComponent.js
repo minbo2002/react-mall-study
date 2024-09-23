66f009020e97151cdc30c30303555c104142d7af
minbo2002 <minbo2002@naver.com>
2024-09-24 03:22:41 +0900

    [feat] Todo List 목록 구현

---
+++ src/components/todo/ListComponent.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { getList } from "../../api/todoApi";
+import useCustomMove from "../../hooks/useCustomMove"; // 공통으로 사용하는 코드는 커스텀 hook으로 만들어서 사용
+
+const initState = {
+    dtoList: [],
+    pageNumList: [],
+    pageRequestDTO: null,
+    prev: false,
+    next: false,
+    totalCount: 0,
+    prevPage: 0,
+    nextPage: 0,
+    totalPage: 0,
+    current: 0
+}
+
+const ListComponent = () => {
+    const { page, size } = useCustomMove();
+
+    const [serverData, setServerData] = useState(initState);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        getList({ page, size }).then(data => {
+            console.log(data);
+            if (data.body) {
+                setServerData(data.body);
+            } else if (data.message) {
+                setError(data.message);
+            }
+        }).catch(error => {
+            console.error("Error fetching todo list:", error);
+            setError("Failed to fetch todo data");
+        });
+    }, [page, size]);
+
+    if (error) {
+        return <div className="text-red-500">{error}</div>;
+    }
+
+    return (
+      <div className="border-2 border-blue-100 mt-10 mr-2 ml-2">
+        <div className="flex flex-wrap mx-auto justify-center p-6">
+            {serverData.dtoList.map(todo =>
+              <div key={todo.id} className="w-full min-w-[400px] p-2 m-2 rounded shadow-md">
+                  <div className="flex">
+                    <div className="font-extrabold text-2xl p-2 w-1/12">
+                        {todo.id}
+                    </div>
+                    <div className="text-1xl m-1 p-2 w-8/12 font-extrabold">
+                        {todo.title}
+                    </div>
+                    <div className="text-1xl m-1 p-2 w-2/12 font-medium">
+                        {todo.dueDate}
+                    </div>
+                  </div>
+              </div>
+            )}    
+        </div>
+      </div>
+    );
+}
+
+export default ListComponent;
--- src/pages/todo/ListPage.js
+++ src/pages/todo/ListPage.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { useSearchParams } from 'react-router-dom';
+import ListComponent from '../../components/todo/ListComponent';
 
-function ListPage(props) {  // todo/IndexPage.js의 <Outlet/> 부분에 렌더링된다
-    
-    const [queryParams] = useSearchParams()  // useSearchParams()는 URL 경로에 있는 '?' 이후의 쿼리스트링을 추출할 때 사용
+const ListPage = () => {
+  return (
+    <div className="p-4 w-full bg-white">
+      <div className="text-3xl font-extrabold">
+          Todo List Page Component
+      </div> 
 
-    const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
-    const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
-
-
-    
-    return (
-      <div className="p-4 w-full bg-white">
-        <div className="text-3xl font-extrabold">
-            Todo List Page Component. page:{page} --- size:{size}
-        </div> 
-      </div>
-    );
+      <ListComponent></ListComponent>
+    </div>
+  );
 }
 
 export default ListPage;
